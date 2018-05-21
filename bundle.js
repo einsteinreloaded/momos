f1920a119db3a3dadf27475d56eaeb607dd4e80b
khubo(aswin) <aswinrajv@gmail.com>
2018-05-21 23:26:10 +0530

    convert to array and replace path with module index

--- bundle.js
+++ bundle.js
@@ -5,13 +5,12 @@ const esprima = require('esprima')
 // entry function
 // @params {String} entryFile
 function bundle(entryFile) {
+  entryFile = path.resolve(__dirname, entryFile)
   const modules = {}
   let id = 0
 
-  entryFile = path.resolve(__dirname, entryFile)
   // recursively reach all modules and load them into modules object
   function fetchModules(file) {
-    console.log('file : ', file)
     if (modules[file] !== undefined || !fs.existsSync(file)) return
 
     let content = fs.readFileSync(file).toString()
@@ -45,16 +44,57 @@ function bundle(entryFile) {
     })
   }
 
+  function addIdToRequires(module) {
+    module.requires.forEach(obj => {
+      obj.id = modules[obj.src].id
+    })
+    return module
+
+  }
+
+  function createLoadPartOflength(id, length) {
+    let string = `loadModule(${id})`
+    let len = string.length
+    let spacesRequired = length - len
+    while (spacesRequired--) {
+      string += ' '
+    }
+    return string
+  }
+
+  function replacePathWithModuleId(module) {
+    if (!module.requires.length) return module.content
+
+    for (let req of module.requires) {
+      let content = module.content
+      let length = req.loc[1] - req.loc[0]
+      let loadPart = createLoadPartOflength(req.id, length);
+      let slice1 = content.slice(0, req.loc[0])
+      let slice2 = content.slice(req.loc[1])
+      module.content = slice1 + loadPart + slice2
+    }
+
+    return module.content
+  }
+
+  function bakeIntoFunction(module) {
+    let code = []
+    let content = replacePathWithModuleId(module)
+
+    code.push('(function(module, exports, loadModule) {')
+    code.push(content)
+    code.push('})')
+    return code.join('\n')
+  }
+
 
   fetchModules(entryFile)
-  let files = Object.keys(modules)
-  for (let file of files) {
-    const module = modules[file]
-    module.requires.forEach(obj => {
-      obj.id = modules[obj.src].id;
-    })
-  }
-  console.log(JSON.stringify(modules))
+  let modulesArray = Object.values(modules)
+    .map(addIdToRequires)
+    .sort((a, b) => a.id - b.id)
+    .map(bakeIntoFunction)
+
+  console.log(JSON.stringify(modulesArray))
 }
 
 
